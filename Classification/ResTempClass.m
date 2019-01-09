cfc675a4cbdae8470e073d29ecf7be31332ad2a2
Mehmet Haklidir <haklidir@gmail.com>
2019-01-09 10:08:06 +0300

    Add files via upload
    Added comments

--- Classification/ResTempClass.m
+++ Classification/ResTempClass.m
@@ -1,3 +1,15 @@
+% This code has been developed for
+% 
+% "Predicting the Geothermal Reservoir Temperatures with Hydrogeochemical Aspect in Western Anatolia (Turkey): A Machine Learning Approach" has been submitted to Computer and Geosciences Journal https://www.journals.elsevier.com/computers-and-geosciences
+% 
+% by Füsun S. Tut Haklıdır, Mehmet Haklıdır
+% 
+% 1 Istanbul Bilgi University, Department of Energy Systems Engineering, Eyüp Istanbul-Turkey
+% 
+% 2 TUBITAK BILGEM, Gebze Kocaeli-Turkey
+% 
+% fusun.tut@bilgi.edu.tr, mehmet.haklidir@tubitak.gov.tr 
+
 %% Training by using SVM%%
 [trainedSVMClassifier, validationAccuracy] = trainSVMClassifier(dataTrainZ);
 %% Training by using KNN%%
